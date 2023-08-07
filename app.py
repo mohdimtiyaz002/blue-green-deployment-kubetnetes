d319da94ab74d36b3747acf7bfa37ac8ef4b76e1
mohdimtiyaz002 <76655817+mohdimtiyaz002@users.noreply.github.com>
2023-08-07 15:38:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
+import argparse
 
 app = Flask(__name__)
 
-@app.route('/ping')
+@app.route('/ping', methods=['GET'])
 def ping():
-    return "Hi, this code is developed by MOHD IMTIYAZ : 1"
+    return "Hi, This code for blue app is develop by Mohd Imtiyaz: 1", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0')
