fe02c2972d169672dc56693b6e5cfe9fa5982f76
mohdimtiyaz002 <76655817+mohdimtiyaz002@users.noreply.github.com>
2023-08-07 16:01:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/ping', methods=['GET'])
 def ping():
-    return "Hi, This code for blue app is develop by Mohd Imtiyaz: 1", 200
+    return "Hi, This code for green app is develop by Mohd Imtiyaz: 2", 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
